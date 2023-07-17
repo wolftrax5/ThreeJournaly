1630a146b3a8483159207b636107cba20251c412
Alejandro Medina <5almero5@gmail.com>
2023-07-17 16:09:30 -0600

    Start tutoriasl

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,43 +1,60 @@
 import * as THREE from 'three'
 
+// SCENE
 const scene = new THREE.Scene()
+/** CAMERA */
+// The Aspect Ratio
+const ASR :number=  window.innerWidth / window.innerHeight;
+// the Field of View
+const FOV:number = 75
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
+    FOV,
+    ASR,
     0.1,
     1000
 )
-camera.position.z = 2
-
+camera.position.z = 4
+/** ******* */
+/** Render */
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
-
+/** ******* */
+/** Object */
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
-
+// a mesh needs a shader and a material
 const cube = new THREE.Mesh(geometry, material)
+/** ******* */
+
 scene.add(cube)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
+    camera.aspect = ASR
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
 
+let time =  Date.now()
+
 function animate() {
-    requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
+    //Time
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time
+    time = currentTime
+    //Update objects
+    cube.rotation.x += 0.001 * deltaTime
+    cube.rotation.y += 0.001 * deltaTime
+    // Render
     render()
+
+    requestAnimationFrame(animate)
 }
 
 function render() {
